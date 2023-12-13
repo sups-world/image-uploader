58706385c30ff6847cf33c500dc1ed45c28bcb8f
Suprajna Sharma <suprasubedi11@gmail.com>
2023-12-13 12:01:28 +0545

    added configs,multer

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,22 @@
+// import { config, uploader } from "cloudinary";
+
+// const cloudinaryConfig = () =>
+//   config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//   });
+// export {cloudinaryConfig,uploader}
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+export { cloudinary };
+
+// //for uploading in cloudinary
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function(error, result) {console.log(result); });
