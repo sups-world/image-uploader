9cb1224b8c64f11f3c6eb512c83d9faa137078ab
Suprajna Sharma <suprasubedi11@gmail.com>
2023-12-13 12:08:06 +0545

    image successfully stored in buffer

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
 import "dotenv/config";
 
+import { multerUploads } from "./multer.js";
 const app = express();
+
+app.post("/upload", multerUploads, (req, res) => {
+  console.log("req.file", req.file);
+  res.send(req.file);
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+//what you get in req.file
+// {
+//   "fieldname": "image",
+//     "originalname": "sample.jpg",
+//     "encoding": "7bit",
+//     "mimetype": "image/jpeg",
+//     "size": 63660
+// }
